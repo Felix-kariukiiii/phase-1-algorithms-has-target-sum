8bef8460060c71edd465dc2c736d144841b7d624
Felix <felix@DESKTOP-F0L63C2.localdomain>
2023-10-25 21:40:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+   for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -8,10 +15,24 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  // iterate over the array of numbers
+  // for the current number, identify a complementary number that adds to our target
+ // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+ // iterate over the remaining numbers in the array
+  // check if any of the remaining numbers is the complement
+      // if so, return true
+      // if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+   // iterate over the array of numbers
+  // for the current number, identify a complementary number that adds to our target
+ // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+ // iterate over the remaining numbers in the array
+  // check if any of the remaining numbers is the complement
+      // if so, return true
+      // if we reach the end of the array, return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -19,16 +40,11 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
   console.log("");
-
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
-
 module.exports = hasTargetSum;
